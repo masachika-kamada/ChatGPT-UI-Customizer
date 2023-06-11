0804999045b9b9ab5077b57ec22d413c6c2e8974
masachika <masachika.kmd@gmail.com>
2023-06-11 19:22:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ChatGPT UI Customizer
+
+ChatGPT Customizer is a Chrome extension that allows you to customize the UI of the ChatGPT website to your liking. With this extension, you can:
+
+- Change the username on the ChatGPT website to 'USER'
+- Remove unnecessary warnings below the text box
+- Make the page scrollbar wider
+
+## Installation
+
+1. Download the latest zip file from the Releases on GitHub.
+2. Unzip the downloaded file.
+3. Open Google Chrome and navigate to `chrome://extensions`.
+4. Enable Developer mode by toggling the switch in the top right corner of the Extensions page.
+5. Click on the `Load unpacked` button and select the unzipped folder.
+6. The extension should now be added to your browser.
+
+## Usage
+
+Once installed, the extension will automatically customize the UI elements of the ChatGPT website as you specified.
+
+## Appearance
+
+![image](https://github.com/masachika-kamada/ChatGPT-WideScreen-Adjuster/assets/63488322/77dbab37-ea5d-4491-a99b-ff4a19415283)
---
+++ script.js
@@ -0,0 +1,46 @@
+function customizePage() {
+  // Change the text of the button
+  // Assuming that the button has an ID starting with "headlessui-menu-button" and the text is in the second div
+  const changeTextSelector = 'button[id^="headlessui-menu-button"] > div:nth-child(2)';
+  const changeTextNode = document.querySelector(changeTextSelector);
+  if (changeTextNode) {
+    changeTextNode.textContent = 'USER';
+  }
+
+  // Remove content using the specified XPath
+  const removeContentXPaths = [
+    '//*[@id="__next"]/div[2]/div[2]/div/main/div[3]/div/span',
+    '//*[@id="__next"]/div[2]/div[2]/div/main/div[3]/div[2]/span'
+  ];
+
+  for (const xpath of removeContentXPaths) {
+    const removeContentNode = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (removeContentNode) {
+      removeContentNode.innerHTML = '';
+    }
+  }
+
+  // Change the width and center-align the target element
+  // Assuming that the target element has the classes "absolute", "bottom-0", and "left-0"
+  const targetElement = document.querySelector('.absolute.bottom-0.left-0');
+  if (targetElement) {
+    targetElement.style.width = 'calc(100% - 0.5rem)';  // Adjust the width considering the scrollbar width
+  }
+}
+
+function observeDOMChanges() {
+  const targetNode = document.body;
+  const config = { childList: true, subtree: true };
+
+  const observer = new MutationObserver((mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+        customizePage();
+      }
+    }
+  });
+
+  observer.observe(targetNode, config);
+}
+
+observeDOMChanges();
